d6ed937fed10c77792ce5211d444f4ad2d0018a0
Marko Krajina <mkrajina@koteks.local>
2021-07-23 10:57:56 +0200

    Removed  job  post

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,18 +6,6 @@ const Services = () => {
   return (
     <section className="section bg-grey">
       <div className="section-center services-grid">
-        <Link to="/o-nama/posao" className="services-link" key="rndasda">
-          <article key={"nsadas"} className="service">
-            <FaBriefcase className="service-icon" />
-            <h4>Natječaj za posao</h4>
-            <div className="underline underline-sm"></div>
-            <p>
-              Osijek Koteks traži stručnjaka za EU projekte. Potrebna je VSS.
-              Dvije godine radnog iskustva i odlično poznavanje engleskog jezika
-              u govoru i pismu.
-            </p>
-          </article>
-        </Link>
         {services.map(service => {
           const { id, icon, title, text, classStyle, link } = service
           return (
